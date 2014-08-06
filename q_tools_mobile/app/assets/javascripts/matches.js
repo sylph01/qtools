fa024aa0361b95352f5b6a42587e9c48d04e92aa
Jun-ya HASEBA <7pairs@gmail.com>
2014-08-06 14:29:29 +0900

    Extract function, changeType()

--- q_tools_mobile/app/assets/javascripts/matches.js
+++ q_tools_mobile/app/assets/javascripts/matches.js
@@ -4,44 +4,19 @@ $(document).ready(function(){
 	$("#player3_freeselect").hide();
 	$("#player4_freeselect").hide();
 	
-	$("#match_player1_type").change(function(){
-		if($("#match_player1_type").val() == "自由"){
-			// activate
-			$("#player1_freeselect").show();
-		} else {
-			// deactivate
-			$("#player1_freeselect").hide();
-		}
-	});
-	
-	$("#match_player2_type").change(function(){
-		if($("#match_player2_type").val() == "自由"){
-			// activate
-			$("#player2_freeselect").show();
-		} else {
-			// deactivate
-			$("#player2_freeselect").hide();
-		}
-	});
-	
-	$("#match_player3_type").change(function(){
-		if($("#match_player3_type").val() == "自由"){
-			// activate
-			$("#player3_freeselect").show();
-		} else {
-			// deactivate
-			$("#player3_freeselect").hide();
-		}
-	});
-	
-	$("#match_player4_type").change(function(){
-		if($("#match_player4_type").val() == "自由"){
-			// activate
-			$("#player4_freeselect").show();
-		} else {
-			// deactivate
-			$("#player4_freeselect").hide();
-		}
-	});
+	$("#match_player1_type").change(changeType);
+	$("#match_player2_type").change(changeType);
+	$("#match_player3_type").change(changeType);
+	$("#match_player4_type").change(changeType);
 });
 
+function changeType() {
+	var type = $(this);
+	if (type.val() == "自由") {
+		// activate
+		type.next("span").show();
+	} else {
+		// deactivate
+		type.next("span").hide();
+	}
+}
